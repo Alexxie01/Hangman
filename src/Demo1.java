01d32b72e6d67bd1a6c583380ad4033fd21bb6dd
Alex Xie <xiealex@hotmail.com>
2018-07-29 00:33:00 -0700

    hangman

---
+++ src/Demo1.java
@@ -0,0 +1,12 @@
+/*
+ * this program will calculate something new 
+ */
+
+public class Demo1 {
+
+	public static void main (String[] args) {
+		System.out.println("hello, this is my first program");
+		//todo this needs unit tests
+
+	}
+}
